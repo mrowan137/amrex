49977f798fe399acf913690f59d1b55b2b939658
Michael Rowan <mrowan137@gmail.com>
2020-04-16 02:33:14 -0400

    Reduced comms (?) in loadbalance

--- Source/Parallelization/WarpXRegrid.cpp
+++ Source/Parallelization/WarpXRegrid.cpp
@@ -30,7 +30,7 @@ WarpX::LoadBalance ()
 
     // By default, do not do a redistribute; this toggles to true if RemakeLevel
     // is called for any level
-    bool doLoadBalance = false;
+    int doLoadBalance = 0;
 
     const int nLevels = finestLevel();
     for (int lev = 0; lev <= nLevels; ++lev)
@@ -50,7 +50,6 @@ WarpX::LoadBalance ()
         //     old_index_to_rank(dm):  pre-gather index --> rank
 
         amrex::Vector<Real> cost_to_send;
-        //amrex::Vector<int> rank_to_send;
         amrex::Vector<int> index_to_send;
 
         MultiFab* Ex = Efield_fp[lev][0].get();
@@ -60,26 +59,29 @@ WarpX::LoadBalance ()
             index_to_send.push_back(mfi.index());
         }
                 
-        amrex::Vector<int> recvcount, disp;
+        amrex::Vector<long> recvcount, disp;
         recvcount.resize(ParallelDescriptor::NProcs(), 0);
+        recvcount.assign(ParallelDescriptor::NProcs(), 0);
         disp.resize(ParallelDescriptor::NProcs(), 0);
+        disp.assign(ParallelDescriptor::NProcs(), 0);
 
-        Real n_cost = cost_to_send.size();
+        int n_cost = cost_to_send.size();
         ParallelDescriptor::Gather(&n_cost, 1,
                                    &recvcount[0], 1,
                                    ParallelDescriptor::IOProcessorNumber());
         
         for (int i=1; i<disp.size(); i++)
         {
-            disp[i] = disp[i-1] + recvcount[i-1] + 1;
+            disp[i] = disp[i-1] + recvcount[i-1];
+            //amrex::Print() << "MyProc=" << ParallelDescriptor::MyProc() << "disp[i]=" << disp[i] << '\n';
         }
         
         amrex::Vector<Real> new_index_to_cost;
-        amrex::Vector<int> new_index_to_rank, new_index_to_old_index;
+        amrex::Vector<int> new_index_to_old_index;
         new_index_to_cost.resize(costs[lev]->size(), 0.0);
-        new_index_to_rank.resize(costs[lev]->size(), 0);
+        new_index_to_cost.assign(costs[lev]->size(), 0.0);
         new_index_to_old_index.resize(costs[lev]->size(), 0);
-        
+        new_index_to_old_index.assign(costs[lev]->size(), 0);
         
         ParallelDescriptor::Gatherv(&cost_to_send[0],
                                     cost_to_send.size(),
@@ -87,14 +89,7 @@ WarpX::LoadBalance ()
                                     recvcount,
                                     disp,
                                     ParallelDescriptor::IOProcessorNumber());
-        
-        // ParallelDescriptor::Gatherv(&rank_to_send[0],
-        //                             rank_to_send.size(),
-        //                             &new_index_to_rank[0],
-        //                             recvcount,
-        //                             disp,
-        //                             ParallelDescriptor::IOProcessorNumber());
-        
+                
         ParallelDescriptor::Gatherv(&index_to_send[0],
                                     index_to_send.size(),
                                     &new_index_to_old_index[0],
@@ -109,13 +104,12 @@ WarpX::LoadBalance ()
             // Invert the new_ind-->old_ind
             Vector<int> old_index_to_new_index;
             old_index_to_new_index.resize(costs[lev]->size(), 0);
+            old_index_to_new_index.assign(costs[lev]->size(), 0);
             for (int i=0; i<old_index_to_new_index.size(); i++)
             {
-                new_index_to_old_index[old_index_to_new_index[i]] = i;
+                old_index_to_new_index[new_index_to_old_index[i]] = i;
             }
         
-            //amrex::Vector<Real> old_index_to_cost;
-            //old_index_to_cost.resize(*costs[lev]->size(), 0.0);
             for (int i=0; i<costs[lev]->size(); i++)
             {
                 (*costs[lev])[i] = new_index_to_cost[old_index_to_new_index[i]];
@@ -128,26 +122,28 @@ WarpX::LoadBalance ()
         amrex::Real currentEfficiency = 0.0;
         // To store efficiency for proposed distribution mapping
         amrex::Real proposedEfficiency = 0.0;
-        const DistributionMapping newdm;
+        DistributionMapping newdm;
         
         if (load_balance_efficiency_ratio_threshold > 0.0
             & ParallelDescriptor::MyProc() == ParallelDescriptor::IOProcessorNumber())
         {
-            // Compute efficiency for the current distribution mapping
+            // // Compute efficiency for the current distribution mapping
             const DistributionMapping& currentdm = DistributionMap(lev);
             ComputeDistributionMappingEfficiency(currentdm, *costs[lev],
                                                  currentEfficiency);
 
-            // Arguments for the load balancing
+            // // Arguments for the load balancing
             const amrex::Real nboxes = costs[lev]->size();
             const amrex::Real nprocs = ParallelContext::NProcsSub();
             const int nmax = static_cast<int>(std::ceil(nboxes/nprocs*load_balance_knapsack_factor));
 
             newdm = (load_balance_with_sfc)
                 ? DistributionMapping::makeSFC(*costs[lev], boxArray(lev), proposedEfficiency, false)
-                : DistributionMapping::makeKnapSack(*costs[lev], proposedEfficiency, nmax);
+                : DistributionMapping::makeKnapSack(*costs[lev], proposedEfficiency, nmax, false);
 
             // Root has all the information to decide
+            //amrex::AllPrint() << ParallelDescriptor::MyProc() << "::Current  efficiency: " << currentEfficiency  << "\n";
+            //amrex::AllPrint() << ParallelDescriptor::MyProc() << "::Proposed efficiency: " << proposedEfficiency << "\n";
             doLoadBalance = (proposedEfficiency > load_balance_efficiency_ratio_threshold*currentEfficiency);
         }
 
@@ -157,13 +153,33 @@ WarpX::LoadBalance ()
         
         if (doLoadBalance)
         {
-            // Broadcastv newdm, or just the vector from which to construct
-            //RemakeLevel(lev, t_new[lev], boxArray(lev), newdm);
+            Vector<int> pmap;
+            pmap.resize(DistributionMap(lev).size(), 0);
+            pmap.assign(DistributionMap(lev).size(), 0);
+            if (ParallelDescriptor::MyProc() == ParallelDescriptor::IOProcessorNumber())
+            {
+                pmap = newdm.ProcessorMap();
+                // For root, get the index array from distribution mapping
+                //for (int i=0; i<pmap.size(); i++)
+                //{
+                // pmap[i] = newdm[i];
+                //}
+            }
+            
+            //Broadcastv newdm, or just the vector from which to construct
+            ParallelDescriptor::Bcast(&pmap[0], pmap.size(), ParallelDescriptor::IOProcessorNumber());
+            if (ParallelDescriptor::MyProc() != ParallelDescriptor::IOProcessorNumber())
+            {
+                newdm = DistributionMapping(pmap);
+            }
+
+            //amrex::AllPrint() << "myproc=" << ParallelDescriptor::MyProc() << ", dm=" << newdm << '\n';
+            RemakeLevel(lev, t_new[lev], boxArray(lev), newdm);
         }
     }
     if (doLoadBalance)
     {
-        //mypc->Redistribute();
+        mypc->Redistribute();
     }
 #endif
 }
